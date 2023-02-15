28a9212d6b88c281921a0e6acc6b88811a896709
Reinhardt <contact@reinhello.tech>
2023-02-15 21:03:35 +0700

    Fix padding

--- components/View.vue
+++ components/View.vue
@@ -4,11 +4,9 @@
   </div>
   <div v-else>
     <div class="flex flex-col items-center justify-center">
+      <h1 class="text-3xl p-5 font-extrabold">{{ fileParam }}</h1>
       <br>
-      <h1 class="text-3xl font-extrabold">{{ fileParam }}</h1>
-      <br>
-      <img class="noselect" ondragstart="return false" draggable="false" :src="'http://localhost:3000/raw/' + fileParam" />
-      <br>
+      <img class="noselect p-5" ondragstart="return false" draggable="false" :src="'http://localhost:3000/raw/' + fileParam" />
       <p>Uploaded by <b>{{ files?.uploader }}</b> at <b>{{ moment(files?.date).format("D/MM/YY, h:mm:ss A") }}</b></p>
     </div>
   </div>
