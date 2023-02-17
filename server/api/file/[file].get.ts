8c8db5529b979038ed6a666ffd72b9da76bce97b
Reinhardt <contact@reinhello.tech>
2023-02-17 16:20:15 +0700

    Add missing property

--- server/api/file/[file].get.ts
+++ server/api/file/[file].get.ts
@@ -20,6 +20,7 @@ export default defineEventHandler(async (event) => {
       id: file.id,
       date: file.date,
       ext: mime.getExtension(file.mimetype),
+      nsfw: file.nsfw,
       uploader: {
         name: file.uploader.name,
       },
